4e33e176e7b5b32fc478f913b835d518404ded58
NotClient01 <spencerdore615@gmail.com>
2025-08-24 11:55:00 -0400

    Delete Drone Web Final directory

--- Drone Web Final/assets/js/main.js
+++
@@ -1,9 +0,0 @@
-// Header background on scroll
-window.addEventListener('scroll', function() {
-    const header = document.querySelector('header');
-    if (window.scrollY > 100) {
-        header.style.background = '#1E3D2F';
-    } else {
-        header.style.background = '#1E3D2F';
-    }
-});
