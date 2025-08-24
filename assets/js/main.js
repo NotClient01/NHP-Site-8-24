658a1de84264c7adee6f17211c8b12a5e8a0a7df
NotClient01 <spencerdore615@gmail.com>
2025-08-24 11:54:35 -0400

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+// Header background on scroll
+window.addEventListener('scroll', function() {
+    const header = document.querySelector('header');
+    if (window.scrollY > 100) {
+        header.style.background = '#1E3D2F';
+    } else {
+        header.style.background = '#1E3D2F';
+    }
+});
